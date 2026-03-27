4202610a8e56fafd971100c9604f81f1e81f8a02
AdityaRathi-10 <adityarathi3916@gmail.com>
2026-03-27 23:22:43 +0530

    role based routes of users

---
+++ lib/navItems.ts
@@ -0,0 +1,22 @@
+import { ROLE } from "@/app/generated/prisma/enums";
+
+interface NavigationProps {
+    label: string
+    href: string
+}
+
+export const NAV_ITEMS: Record<ROLE, NavigationProps[]> = {
+    STUDENT: [
+        { label: "Dashboard", href: "/complaints" },
+        { label: "My Complaints", href: "/complaints/me" },
+        { label: "Create Complaint", href: "/complaints/new" }
+    ],
+    CARETAKER: [
+        { label: "Dashboard", href: "/complaints" },
+        { label: "My Tasks", href: "/complaints/me" }
+    ],
+    SUPERVISOR: [
+        { label: "Dashboard", href: "/complaints" },
+        { label: "Leaderboard", href: "/leaderboard" }
+    ]
+}
