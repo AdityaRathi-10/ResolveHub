8e5495ccda132d5685db2400abfb15de3f04be4d
AdityaRathi-10 <adityarathi3916@gmail.com>
2026-03-29 20:29:28 +0530

    added option to allow only students to upvote

--- components/Upvotes.tsx
+++ components/Upvotes.tsx
@@ -3,19 +3,19 @@
 import { toggleUpvote } from "@/app/complaints/[id]/action";
 import { ChevronUp } from "lucide-react";
 import { useState } from "react";
+import { Button } from "./ui/button";
 
 interface UpvoteProps {
     complaintId: string
     upvotesCount: number
     hasUpvoted: boolean
+    isStudent: boolean
 }
 
-export default function UpVotes({ complaintId, upvotesCount, hasUpvoted }: UpvoteProps) {
+export default function UpVotes({ complaintId, upvotesCount, hasUpvoted, isStudent }: UpvoteProps) {
     const [count, setCount] = useState(upvotesCount)
     const [liked, setLiked] = useState(hasUpvoted)
     const [isLiking, setIsLiking] = useState(false)
-
-    console.log("l", liked)
 
     const handleUpvote = async () => {
         if(!liked) {
@@ -41,14 +41,14 @@ export default function UpVotes({ complaintId, upvotesCount, hasUpvoted }: Upvot
         <div>
             <p className="text-xs text-muted-foreground mb-1.5">Upvotes</p>
             <div className="flex items-center gap-1.5">
-                <button 
-                    className={`flex items-center gap-1.5 text-sm font-semibold cursor-pointer text-foreground ${liked ? "hover:bg-green-300" : "hover:bg-primary/10"} hover:text-primary transition-colors px-2.5 py-1 rounded-lg border border-border ${liked ? "bg-green-200" : "bg-muted"}`}
+                <Button 
+                    className="flex items-center gap-1.5 text-sm font-semibold cursor-pointer text-foreground hover:bg-primary/10 hover:text-primary transition-colors px-2.5 py-1 rounded-lg border border-border bg-muted"
                     onClick={handleUpvote}
-                    disabled={isLiking}
+                    disabled={isLiking || !isStudent}
                 >
                     <ChevronUp className="h-4 w-4" />
                     {count}
-                </button>
+                </Button>
             </div>
         </div>
     )
