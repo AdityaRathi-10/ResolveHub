4c37c1adabe3ae457e9e2f167b877962239beaaf
AdityaRathi-10 <adityarathi3916@gmail.com>
2026-03-29 20:27:01 +0530

    add props to themeprovider and import sora font family in global css

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -4,10 +4,11 @@ import React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 
 export function ThemeProvider({
-    children
+    children,
+    ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
     return (
-        <NextThemesProvider>
+        <NextThemesProvider {...props}>
             {children}
         </NextThemesProvider>
     )
