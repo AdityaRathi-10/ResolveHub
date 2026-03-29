f3fecf70a2c2552d987c47f3307298d601399af5
AdityaRathi-10 <adityarathi3916@gmail.com>
2026-03-29 20:36:19 +0530

    action for caretaker assign, unassign and start work on complaint with audit insertion

--- app/complaints/[id]/action.ts
+++ app/complaints/[id]/action.ts
@@ -3,6 +3,7 @@
 import { prisma } from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/options";
+import { revalidatePath } from "next/cache";
 
 export async function toggleUpvote(complaintId: string) {
   const session = await getServerSession(authOptions);
@@ -53,5 +54,134 @@ export async function toggleUpvote(complaintId: string) {
     });
 
     return { hasUpvoted: true };
+  }
+}
+
+export async function assignComplaint(complaintId: string) {
+  const session = await getServerSession(authOptions);
+  if (!session?.user) throw new Error("Unauthorized");
+
+  const userId = session.user.id;
+  const username = session.user.name;
+
+  if (session.user.role !== "CARETAKER") {
+    throw new Error("Only caretakers can assign complaints");
+  }
+
+  const updated = await prisma.complaint.updateMany({
+    where: {
+      id: complaintId,
+      assignedToId: null,
+    },
+    data: {
+      assignedToId: userId,
+    },
+  });
+
+  if (updated.count === 0) {
+    return {
+      success: false,
+      message: "Already assigned",
+    };
+  }
+
+  await prisma.complaintAudit.create({
+    data: {
+      complaintId,
+      actorId: userId,
+      type: "ASSIGNED",
+      message: `Assigned to ${username}`
+    }
+  })
+
+  return {
+    success: true,
+    message: "Complaint assigned successfully",
+  };
+}
+
+export async function unassignComplaint(complaintId: string) {
+  const session = await getServerSession(authOptions);
+  if (!session?.user) throw new Error("Unauthorized");
+
+  const userId = session.user.id;
+  const username = session.user.name;
+
+  if (session.user.role !== "CARETAKER") {
+    throw new Error("Only caretakers can unassign complaints");
+  }
+
+  const updated = await prisma.complaint.updateMany({
+    where: {
+      id: complaintId,
+      assignedToId: userId,
+    },
+    data: {
+      assignedToId: null,
+    },
+  });
+
+  if (updated.count === 0) {
+    return {
+      success: false,
+      message: "Already unassigned",
+    };
+  }
+
+  await prisma.complaintAudit.create({
+    data: {
+      complaintId,
+      actorId: userId,
+      type: "UNASSIGNED",
+      message: `Unassigned ${username}`
+    }
+  })
+
+  return {
+    success: true,
+    message: "Complaint unassigned successfully",
+  };
+}
+
+export async function startWorkingOnComplaint(complaintId: string) {
+  const session = await getServerSession(authOptions);
+  if (!session?.user) throw new Error("Unauthorized");
+
+  const userId = session.user.id;
+
+  if (session.user.role !== "CARETAKER") {
+    throw new Error("Only caretakers can start working on complaints");
+  }
+
+  const updated = await prisma.complaint.update({
+    where: {
+      id: complaintId
+    },
+    data: {
+      status: "IN_PROGRESS"
+    }
+  })
+
+  if(!updated) {
+    return {
+      success: false,
+      message: "Error updating status"
+    }
+  }
+
+  await prisma.complaintAudit.create({
+    data: {
+      complaintId,
+      actorId: userId,
+      type: "STATUS_CHANGED",
+      message: `Complaint is now ${updated.status.replaceAll("_", " ")} state`
+    }
+  })
+
+  revalidatePath(`/complaints/${complaintId}`)
+
+  return {
+    success: true,
+    message: "Complaint is now in progress"
   }
 }
